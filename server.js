952efa2cc6fdddd16566689f5213f4a27220990d
AndyJ <andrewja01@gmail.com>
2019-07-07 22:42:59 -0700

    connecting to mongodb

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 app.get('/', (req, res) => res.send('API running'));
+
+//Connect Database
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
